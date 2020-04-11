5189457718e6745e74bda921b190f62bc5f944a1
arichards4814 <arichards4814@gmail.com>
2020-04-11 08:21:20 -0400

    trying to get active storage to work

--- app/controllers/curriculumstags_controller.rb
+++ app/controllers/curriculumstags_controller.rb
@@ -1,2 +1,21 @@
 class CurriculumstagsController < ApplicationController
+
+    def create 
+        currtag = Curriculumstag.new(curr_tag_params)
+
+        if currtag.save
+            render json: currtag
+        else 
+            render json: {errors: currtag.errors.full_messages}
+        end
+    end
+
+    private
+
+
+
+    def curr_tag_params
+        params.require(:curriculumstag).permit(:curriculum_id, :tag_id)
+    end
+
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,8 @@
 class TagsController < ApplicationController
    
+    def create
+       tag = Tag.find_or_create_by(name: params[:name])
+    
+        render json: tag
+    end
 end
